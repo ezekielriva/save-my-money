50a9bc927042a5c3a53e5a5c502bd2d90bd50285
Ezekiel <ezerivadeneiral@gmail.com>
2014-04-21 20:26:11 -0300

    Adds views

--- spec/models/chest_spec.rb
+++ spec/models/chest_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Chest do
   let(:user) { create(:user) }
-  
+
   describe "Validations" do
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of(:name).scoped_to(:user_id) }
