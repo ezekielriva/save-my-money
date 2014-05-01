cbcbd1c1f80ea4d5d94d02f11f611c1e6c264f6f
Ezekiel <ezerivadeneiral@gmail.com>
2014-05-01 18:03:18 -0300

    Adds spec to coin methods.

--- spec/models/coin_spec.rb
+++ spec/models/coin_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Coin do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#from_recurrent" do
+    before(:each) do
+      @coin = create(:coin)
+      @coin.should_receive(:parent).and_return( build(:coin) )
+    end
+
+    it { expect(@coin.from_recurrent).to be_true }
+  end
 end
